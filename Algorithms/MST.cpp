7f1d0ec5619c30a3a41c2e343a8c12ce9e764c00
naresh0839 <naresh0839@gmail.com>
2020-05-26 03:13:19 +0530

    commiting Minimum Spanning tree to git

---
+++ Algorithms/MST.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 100005
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+}edges[MAXN];
+
+int n, m; 
+vector<int> tree_id(MAXN);
+vector<Edge> result;
+
+int main(){
+    cin >> n >> m;
+    // n == number of vertices
+    // m == no of edges in graph
+    for(int i = 0; i < m; ++i){
+        cin >> edges[i].u >> edges[i].v >> edges[i].weight;
+    }
+    sort(edges, edges + m);
+    for (int i = 0; i < n; i++){
+        tree_id[i] = i;
+    }
+    for (Edge e : edges) {
+        if (tree_id[e.u] != tree_id[e.v]) {
+            cost += e.weight;
+            result.push_back(e);
+            
+            int old_id = tree_id[e.u], new_id = tree_id[e.v];
+            for (int i = 0; i < n; i++) {
+                if (tree_id[i] == old_id)
+                    tree_id[i] = new_id;
+            }
+        }
+    }    
+    return 0;
+}
+int cost = 0;
+
+
+
+
+
+
