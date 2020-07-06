1e24eb82707acc300f457d36067f82abe688d268
naresh0839 <naresh0839@gmail.com>
2020-07-06 20:26:00 +0530

    prefix function commit to git

---
+++ Algorithms/prefixfunction.cpp
@@ -0,0 +1,16 @@
+vector<int> pref_func(string &s) {
+    int n = s.length();
+    vector<int> pi(n);
+    pi[0] = 0;
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (s[i] != s[j] and j != 0) {
+            j = pi[j - 1];
+        }
+        if (s[i] == s[j]) {
+            j++;
+        }
+        pi[i] = j;
+    }
+    return pi;
+}
