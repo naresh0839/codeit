0d701c3be117649ef729f30bc33cd2c0e847347e
naresh0839 <naresh0839@gmail.com>
2020-10-27 19:08:56 +0530

    asdf

--- Algorithms/syntax.cpp
+++ Algorithms/syntax.cpp
@@ -5,28 +5,34 @@ using namespace std;
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0)
-#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rep(i, b) for (int i = 0; i < b; i++)
 #define repn(i, n) for (int i = 1; i <= n; i++)
 #define rrep(i, b, a) for (int i = b; i >= a; i--)
 #define sz(v) (v.empty() ? 0 : v.size())
 #define all(v) v.begin(), v.end()
-#define mem0(a) memset(a, 0, sizeof(a))
-#define mem1(a) memset(a, -1, sizeof(a))
+#define mem(a, val) memset(a, val, sizeof(a))
 #define trav(a, x) for (auto &a : x)
 #define endl '\n'
 #define fi first
 #define se second
 #define pb push_back
 #define int long long
-#define mod 1000000007
-#define rem 998244353
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define time__(d) for (long blockTime = 0; (blockTime == 0 ? (blockTime = clock()) != 0 : false); debug("%s time : %.4fs", d, (double)(clock() - blockTime) / CLOCKS_PER_SEC))
 #define maxn 500005
+#define mod 1000000007
+#define N 1005
 const int INF = 1e18;
 typedef pair<int, int> pii;
 
+void doit() {
+    
+}
 signed main() {
     boostIO;
-
+    int tt;
+    cin >> tt;
+    while (tt--) doit();
     return 0;
 }
 // XD
